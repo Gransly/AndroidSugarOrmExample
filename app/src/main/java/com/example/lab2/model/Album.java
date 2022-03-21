8a170462771955fc306b65a3797503588a9d5438
Sergey Antonyuk <bob031299@gmail.com>
2022-03-21 22:31:01 +1000

    create layout for album

---
+++ app/src/main/java/com/example/lab2/model/Album.java
@@ -0,0 +1,33 @@
+package com.example.lab2.model;
+
+import com.orm.SugarRecord;
+
+public class Album extends SugarRecord<Album> {
+    
+    private String title;
+    private Artist artist;
+
+    public Album(String title, Artist artist) {
+        this.title = title;
+        this.artist = artist;
+    }
+
+    public Album() {
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Artist getArtist() {
+        return artist;
+    }
+
+    public void setArtist(Artist artist) {
+        this.artist = artist;
+    }
+}
